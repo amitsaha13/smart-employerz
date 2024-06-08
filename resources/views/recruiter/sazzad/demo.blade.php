7f2709991599885a3b9d0be71b80dd67c74794c1
netcodensazzadali <netcodensazzadali@gmail.com>
2024-06-08 16:35:49 +0600

    project setup for frontend

---
+++ resources/views/recruiter/sazzad/demo.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.recruiter_layout')
+<title>Create New Job | Smartemployerz</title>
+
+@section('content')
+<div class="dashboard custom-space pt-0 pb-0">
+<h3>Me from demo</h3>
+</div>
+
+@endsection
